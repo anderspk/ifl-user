09344fd3b475a626d627f16b686767c010af8f07
anderspk <39174017+anderspk@users.noreply.github.com>
2020-01-28 16:00:57 +0100

    Update Players.js

--- src/components/Players.js
+++ src/components/Players.js
@@ -7,8 +7,8 @@ class Players extends Component {
   state = { players: [] };
 
   componentWillMount() {
-    axios.get('https://case-person.herokuapp.com/showPlayers').then(response => this.setState({ players: response.data }));
-    axios.get('https://case-team.herokuapp.com/showAllTeamData').then(response => this.setState({ teams: response.data }));
+    axios.get('https://crudpi.io/7b4b03/players').then(response => this.setState({ players: response.data }));
+    axios.get('https://crudpi.io/7b4b03/teams').then(response => this.setState({ teams: response.data }));
   }
 
   renderPlayersList = () => {
@@ -16,11 +16,11 @@ class Players extends Component {
       return (
       <tr key={i} className='table-row'>
           <td className='player-name'>
-            <Link to={`/players/${player.player_id}`}>{`${player.first_name} ${player.last_name}`}</Link>
+            <Link to={`/players/${player.id}`}>{`${player.first_name} ${player.last_name}`}</Link>
           </td>
 
           <td className={player.normal_position.toLowerCase()}>{player.normal_position}</td>
-          <td className='team-name'>{this.state.teams.find(team => team.team_id === player.team_id).association_name}</td>
+          <td className='team-name'>{this.state.teams.find(team => team.id === player.team_id).association_name}</td>
       </tr>)
     });
   }
